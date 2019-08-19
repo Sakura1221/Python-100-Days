ee5cf373e8c78f2d9ef07367775a55a0059f2011
Sakura1221 <1442182454@qq.com>
2019-08-20 00:08:08 +0800

    数据类型基础练习题

--- Day01-15/My code/Day07/marquee.py
+++ Day01-15/My code/Day07/marquee.py
@@ -1,9 +1,6 @@
 """
 流水灯程序
 
-Version: 0.1
-Author: 骆昊
-Date: 2018-03-06
 """
 
 import os
@@ -14,10 +11,10 @@ def main():
     str = 'Welcome to 1000 Phone Chengdu Campus      '
     while True:
         print(str)
-        time.sleep(0.2)
-        str = str[1:] + str[0:1]
+        time.sleep(0.2) # 暂停0.2s
+        str = str[1:] + str[0:1] # 实现字符串循环
         # for Windows use os.system('cls') instead
-        os.system('clear')
+        os.system('cls') # 必须在终端运行才有清屏效果
 
 
 if __name__ == '__main__':
--- Day01-15/code/Day07/avgscore.py
+++ Day01-15/code/Day07/avgscore.py
@@ -1,25 +1,18 @@
 """
 输入学生考试成绩计算平均分
-
-Version: 0.1
-Author: 骆昊
-Date: 2018-03-06
+用列表保存名字和分数
 """
 
 
 def main():
-    number = int(input('请输入学生人数: '))
-    names = [None] * number
-    scores = [None] * number
-    for index in range(len(names)):
-        names[index] = input('请输入第%d个学生的名字: ' % (index + 1))
-        scores[index] = float(input('请输入第%d个学生的成绩: ' % (index + 1)))
-    total = 0
-    for index in range(len(names)):
-        print('%s: %.1f分' % (names[index], scores[index]))
-        total += scores[index]
-    print('平均成绩是: %.1f分' % (total / number))
-
+    number = int(input('请输入学生人数：'))
+    name = [None] * number # 用空list储存数据
+    score = [None] * number
+    for index in range(number):
+        name[index] = input('请输入第%d个学生的名字：' % (index+1))
+        score[index] = int(input('请输入第%d个学生的成绩：' % (index+1)))
+    avgscore = sum(score) / number
+    print('平均分为：%.2f' % avgscore)
 
 if __name__ == '__main__':
     main()
